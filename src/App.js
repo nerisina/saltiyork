e6fa5a60b4c0bcb8999e46590ca62cbf6f8f9538
nerisina <neri.esparza@gmail.com>
2020-07-03 21:20:05 -0400

    Lost in Saltillo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Intro = () => (
+  <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
+    <div className="mb3 mb4-ns">
+      Perdido en la ciudad de Saltillo, Coahuila .
     </div>
-  );
-}
+    <div>
+      Aqui podras encontrar algunas recomendaciones de restaurantes, museos y galerias
+      Saltillo es una hermosa ciudad en el Norte de México que si sabes buscar encuentras lugares divertidos.
+      Dattebayo!
+    </div>
+  </div>
+);
+
+const Quote = () => (
+  <div>
+    <Highlight>Back</Highlight> of the <Highlight>net</Highlight>!
+  </div>
+)
+
+const Highlight = props => <span className="highlight">{props.children}</span>
+
+
+const App = () => (
+  <div>
+    <div className="min-vh-100 ph4 flex flex-column">
+      {/* our navigation component */}
+      <Intro />
+    </div>
+    <div className="flex flex-wrap container">{/* our attractions list component */}</div>
+  </div>
+)
 
 export default App;
